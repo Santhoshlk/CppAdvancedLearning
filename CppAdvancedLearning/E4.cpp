c4dd78afd441ce8925236d08992732dec47d0f62
Santhoshlk <santhoshlukka123@gmail.com>
2026-07-31 23:48:46 +0530

    Concurrency project -1 Update Data and show it using multiple thread

--- CppAdvancedLearning/E4.cpp
+++ CppAdvancedLearning/E4.cpp
@@ -1,32 +1,32 @@
-#include <thread>
-#include <mutex>
-std::mutex mtx1;
-
-int x{ 0 };
-
-void func()
-{
-    while (x == 0)
-    {
-        std::lock_guard lck(mtx1);
-       if(x!=1)
-        x = 1 - x;
-    }
-}
-
-void func2()
-{
-    while (x == 0)
-    {
-        std::lock_guard lck(mtx1);
-        if (x != 1)
-          x = 1 - x;
-    }
-}
-
-int main(void)
-{
-    std::jthread thr(func);
-    std::jthread thr2(func2);
-
-}
+//#include <thread>
+//#include <mutex>
+//std::mutex mtx1;
+//
+//int x{ 0 };
+//
+//void func()
+//{
+//    while (x == 0)
+//    {
+//        std::lock_guard lck(mtx1);
+//       if(x!=1)
+//        x = 1 - x;
+//    }
+//}
+//
+//void func2()
+//{
+//    while (x == 0)
+//    {
+//        std::lock_guard lck(mtx1);
+//        if (x != 1)
+//          x = 1 - x;
+//    }
+//}
+//
+//int main(void)
+//{
+//    std::jthread thr(func);
+//    std::jthread thr2(func2);
+//
+//}
