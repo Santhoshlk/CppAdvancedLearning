ac6511ef90e133fd09cfcedc78ea6b6e07dc14bd
Santhoshlk <santhoshlukka123@gmail.com>
2026-07-31 22:34:43 +0530

    Mutexes are completed.

---
+++ CppAdvancedLearning/LiveLock.cpp
@@ -0,0 +1,48 @@
+//#include <iostream>
+//#include <thread>
+//#include <mutex>
+//
+//std::mutex mtx1;
+//std::mutex mtx2;
+//
+//
+//void func()
+//{
+//    bool locked = false;
+//
+//    while (!locked)
+//    {
+//        std::unique_lock lck(mtx1);
+//        std::cout << "Hello Thread 1" << std::endl;
+//        locked = mtx2.try_lock();
+//    }
+//    if (locked)
+//    {
+//        std::cout << "Thread 1 has locked both the mutexes" << std::endl;
+//        mtx2.unlock();
+//
+//    }
+//}
+//
+//
+//void func2() {
+//    bool locked = false;
+//
+//    while (!locked)
+//    {
+//        std::unique_lock lck(mtx2);
+//        std::cout << "Hello Thread 2" << std::endl;
+//        locked = mtx1.try_lock();
+//    }
+//    if (locked)
+//    {
+//        std::cout << "Thread 2 has locked both the mutexes" << std::endl;
+//        mtx1.unlock();
+//    }
+//}
+//
+//int main(void)
+//{
+//    std::jthread thr(func);
+//    std::jthread thr2(func2);
+//}
---
+++ CppAdvancedLearning/LiveLockPractical.cpp
@@ -0,0 +1,25 @@
+//#include <iostream>
+//#include <thread>
+//#include <mutex>
+//
+//std::mutex mtx1;
+//std::mutex mtx2;
+//
+//
+//// the best way to fix livelock is to actually do deadlock  avoidance correctly
+//void func()
+//{
+//    std::scoped_lock lck(mtx1, mtx2);
+//    std::cout << "Thread 1 has locked the mutexes" << std::endl;
+//}
+//
+//
+//void func2() {
+//    {
+//        
+//        std::scoped_lock lck(mtx2, mtx1);
+//        std::cout << "Thread 2 has locked the mutexes" << std::endl;
+//    }
+//
+//
+//
