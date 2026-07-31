73af7cb49e022ef3598e30d0e9e12e53a86bb408
Santhoshlk <santhoshlukka123@gmail.com>
2026-07-31 17:24:14 +0530

    DeadLock as a topic is completely completed.

---
+++ CppAdvancedLearning/DeadLock_Practice.cpp
@@ -0,0 +1,48 @@
+//#include <iostream>
+//#include <thread>
+//#include <mutex>
+//
+//
+//std::mutex mtx1;
+//std::mutex mtx2;
+//
+//void func1()
+//{
+//    std::unique_lock lck1(mtx1, std::defer_lock);
+//    std::unique_lock lck2(mtx2, std::defer_lock);
+//
+//    std::lock(lck1, lck2);
+//
+//    // critical section
+//    std::cout << "There is now no possibility of deadlock" << std::endl;
+//
+//    lck1.unlock();
+//    std::cout << "I unlocked the first lock of the thread 1" << std::endl;
+//
+//    lck2.unlock();
+//    std::cout << "I unlocked the second lock of the thread 1" << std::endl;
+//
+//}
+//
+//void func2()
+//{
+//    std::unique_lock lck1(mtx1, std::defer_lock);
+//    std::unique_lock lck2(mtx2, std::defer_lock);
+//
+//    std::lock(lck2, lck1);
+//
+//    // critical section
+//    std::cout << "There is now no possibility of deadlock" << std::endl;
+//
+//    lck1.unlock();
+//    std::cout << "I unlocked the first lock of the thread 2" << std::endl;
+//
+//    lck2.unlock();
+//    std::cout << "I unlocked the second lock of the thread 2" << std::endl;
+//}
+//
+//int main(void)
+//{
+//    std::jthread thr(func1);
+//    std::jthread thr2(func2);
+//}
