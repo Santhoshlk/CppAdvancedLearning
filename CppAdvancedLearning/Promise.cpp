c9838bb4da3aedfdb3430dc95374e9da28434b50
Santhoshlk <santhoshlukka123@gmail.com>
2026-08-08 19:46:42 +0530

    Futures and Promises Completed

---
+++ CppAdvancedLearning/Promise.cpp
@@ -0,0 +1,31 @@
+//#include <iostream>
+//#include <thread>
+//#include <future>
+//#include <chrono>
+//
+//
+//void Consumer(std::shared_future<int>& ft)
+//{
+//    int a = ft.get();
+//
+//    std::cout << a << std::endl;
+//
+//}
+//
+//void Producer(std::promise<int>& pr)
+//{
+//
+//    pr.set_value(20);
+//}
+//
+//int main(void)
+//{
+//    std::promise<int> p;
+//    std::shared_future<int> f1 = p.get_future();
+//
+//    std::shared_future<int> f2 = f1;
+//
+//    std::jthread thr(Consumer, std::ref(f1));
+//    std::jthread thr2(Consumer, std::ref(f2));
+//    std::jthread thr3(Producer, std::ref(p));
+//}
